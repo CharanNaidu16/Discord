2e8c4ec4acc8fe30528e7a116e3615d65623c8f6
CharanNaidu16 <charanbabu540@gmail.com>
2025-01-04 13:53:59 +0530

    minor change

--- discord/settings.py
+++ discord/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ AUTH_USER_MODEL= 'discord_app.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,19 +122,31 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os
+from pathlib import Path
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
-
-STATIC_DIRS = [os.path.join(BASE_DIR,'discord_app/static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
 
 STATICFILES_DIR = [
     BASE_DIR/'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+# Static files (CSS, JavaScript, images)
+STATIC_URL = '/static/'
+
+# This is where Django will collect static files during the deployment process
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Static directories to be included in `collectstatic`
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'discord_app/static'),  # Path to your app's static files
+    BASE_DIR / 'static',  # Additional static directory
+]
+
+# Media files (uploads by users)
+MEDIA_URL = '/images/'  # Accessible via /media/ URL
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')  
+
 
 
 # Default primary key field type
