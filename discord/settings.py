1586ba87b927c7c6d815b565e00ba46f6b257475
CharanNaidu16 <charanbabu540@gmail.com>
2025-01-04 15:10:37 +0530

    allowed host

--- discord/settings.py
+++ discord/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)12%jhl#zz#82l3m!%4*@=f=^fyhefzqn!8n&w8ovc@5*1jz3n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['discord-ptwy.vercel.app']
 
 
 # Application definition
