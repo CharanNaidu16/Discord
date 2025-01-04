8562792e01e20e1bea1a8e4217ad8710ff632dbb
CharanNaidu16 <charanbabu540@gmail.com>
2025-01-04 13:37:28 +0530

    changes in wsgi

--- discord/settings.py
+++ discord/settings.py
@@ -119,8 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
+
+STATIC_DIRS = [os.path.join(BASE_DIR,'discord_app/static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
 
 STATICFILES_DIR = [
     BASE_DIR/'static'
